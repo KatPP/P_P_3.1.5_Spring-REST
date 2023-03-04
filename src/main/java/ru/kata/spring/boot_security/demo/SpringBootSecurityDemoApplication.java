cdec00a1eb5d77619c4bdbf2a61023a2ff9cba2d
zenit1925 <kostia-85-85@yandex.ru>
2023-03-04 11:28:53 +0300

    Task 3.1.5 Убрал в main создание новых юзеров при запуске, что бы не было ошибки при следующем запуске т.к БД подключена через Docker

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -12,30 +12,30 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 public class SpringBootSecurityDemoApplication {
 
     public static void main(String[] args) {
-        //		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-        ApplicationContext context = SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-        try {
-            UserService userService = context.getBean(UserService.class);
-            RoleService roleService = context.getBean(RoleService.class);
+        	SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+        //   ApplicationContext context = SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+      // try {
+      //     UserService userService = context.getBean(UserService.class);
+      //     RoleService roleService = context.getBean(RoleService.class);
 
-            Role user = new Role("ROLE_USER", 1);
-            Role admin = new Role("ROLE_ADMIN", 2);
+      //     Role user = new Role("ROLE_USER", 1);
+      //     Role admin = new Role("ROLE_ADMIN", 2);
 
-            roleService.save(user);
-            roleService.save(admin);
+      //     roleService.save(user);
+      //     roleService.save(admin);
 
-            User user1 = new User("User", "Ivan", 33, "user@mail.ru", "200");
-            User user2 = new User("Admin", "Nikolay", 45, "admin@mail.ru", "200");
+      //     User user1 = new User("User", "Ivan", 33, "user@mail.ru", "200");
+      //     User user2 = new User("Admin", "Nikolay", 45, "admin@mail.ru", "200");
 
 
-            user1.addRole(roleService.findByName("ROLE_ADMIN"));
-            user2.addRole(roleService.findByName("ROLE_USER"));
-            user2.addRole(roleService.findByName("ROLE_ADMIN"));
+      //     user1.addRole(roleService.findByName("ROLE_ADMIN"));
+      //     user2.addRole(roleService.findByName("ROLE_USER"));
+      //     user2.addRole(roleService.findByName("ROLE_ADMIN"));
 
-            userService.saveUser(user1);
-            userService.saveUser(user2);
+      //     userService.saveUser(user1);
+      //     userService.saveUser(user2);
 
-        } catch (Exception ignored) {
-        }
+      // } catch (Exception ignored) {
+      // }
     }
 }
